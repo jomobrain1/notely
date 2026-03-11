4fe191036af4f23e7b2afabfda1f5aacdd65adfd
jomobrain1 <jomobrain1@gmail.com>
2026-03-11 22:25:15 +0300

    feat(auth): add basic login and register page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaSignInAlt, FaUser } from "react-icons/fa";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      {" "}
+      <section className="heading">
+        <h3>
+          <FaSignInAlt size={30} /> Login
+        </h3>
+        <p> Welcome back</p>
+      </section>
+      <form onSubmit={onSubmit}>
+        <section className="form">
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </section>
+      </form>
+    </>
+  );
 };
 
 export default Login;
