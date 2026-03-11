5d8eca1195b41676ce5988c8f9b4c3eb7a20c7a0
jomobrain1 <jomobrain1@gmail.com>
2026-03-12 01:14:01 +0300

    ref(frontend): finish login

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../store/authSlice.js";
+import authReducer from "../store/auth/authSlice.js";
 
 export const store = configureStore({
   reducer: {
