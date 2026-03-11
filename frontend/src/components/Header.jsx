82ef6e6fffdf28623df31852b8fe911e1c23bab8
jomobrain1 <jomobrain1@gmail.com>
2026-03-11 21:58:41 +0300

    feat(fronted):add routes

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Notely</Link>
+      </div>
+      <ul>
+        <>
+          <li>
+            <Link to="/login">
+              <FaSignInAlt /> Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register">
+              <FaUser /> Register
+            </Link>
+          </li>
+        </>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
